d62511ce8afcb5c534742c369f669a42179afe68
AlexBurnQ <49692389+AlexBurnQ@users.noreply.github.com>
2019-04-16 21:58:07 +0300

    TOPNEW

--- mail.php
+++ mail.php
@@ -1,13 +1,13 @@
 <?php
 
 $recepient = "superqiwiburn@gmail.com";
-$siteName = "SuperQIWI";
+$siteName = "QiwiCash";
 
 // $name = trim($_POST["name"]);
 $phone = trim($_POST["phone"]);
 $message = "Контакты: $phone";
 
-$pagetitle = "Заявка с сайта \"$siteName\"";
+$pagetitle = "Заявка с сайта \"$recepient\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
 
 ?>
