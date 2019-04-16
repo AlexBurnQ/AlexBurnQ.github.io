442e51be4e01867f4f0b387f20406aa8c5a0951f
AlexBurnQ <49692389+AlexBurnQ@users.noreply.github.com>
2019-04-16 20:22:30 +0300

    innit comit

---
+++ mail.php
@@ -0,0 +1,13 @@
+<?php
+
+$recepient = "superqiwiburn@gmail.com";
+$siteName = "SuperQIWI";
+
+// $name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$message = "Контакты: $phone";
+
+$pagetitle = "Заявка с сайта \"$siteName\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+
+?>
