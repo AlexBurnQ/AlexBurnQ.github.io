9b7629e54a3b13f0e352c29d5a48285119ebd46e
AlexBurnQ <49692389+AlexBurnQ@users.noreply.github.com>
2019-04-17 11:45:50 +0300

    форма заявок

--- mail.php
+++ mail.php
@@ -1,13 +1,48 @@
 <?php
 
-$recepient = "superqiwiburn@gmail.com";
-$siteName = "QiwiCash";
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'burncash@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'N#^$3ksc3O0IGK83BK8'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('burncash@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('superqiwiburn@gmail.com');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка с сайта';
+$mail->Body    = '' .$name . ' оставил заявку, его данные ' .$phone. '<br>Почта этого пользователя: ' .$email;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+    header('location: thank-you.html');
+}
 
 // $name = trim($_POST["name"]);
 $phone = trim($_POST["phone"]);
 $message = "Контакты: $phone";
 
-$pagetitle = "Заявка с сайта \"$recepient\"";
+$pagetitle = "Заявка с сайта \"$siteName\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
 
 ?>
